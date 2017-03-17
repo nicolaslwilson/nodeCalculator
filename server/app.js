0e7260c24ad9c9efba564f06e9252f5951a7e6af
Nic Wilson <nicwilson@gmail.com>
2017-03-17 17:15:46 -0500

    Add figlet, calculator functionality

--- server/app.js
+++ server/app.js
@@ -21,6 +21,14 @@ app.post('/calculate', function (req, res) {
   res.send({result: math[operator](leftOperand,rightOperand)});
 });
 
+app.post('/figlet', function (req, res) {
+  console.log('figlet hit', req);
+  figlet.text(req.body.text, {font: 'Larry 3D', horizontalLayout: 'default', verticalLayout: 'default'}, function (err,data) {
+    console.log(data);
+    res.send(data);
+  });
+});
+
 app.listen(app.get("port"), function ( ) {
     console.log("Listening on port: ", "\n" + figlet.textSync(app.get("port"), {font: "3D-ASCII"}));
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,20 +1,115 @@
+var leftOperand = "", rightOperand = "", operator;
+
 $(document).ready(function() {
-  $('.calculate').on('click', function () {
-    var leftOperand = $('#leftOperand').val();
-    var rightOperand = $('#rightOperand').val();
-    var operator = $(this).data("operator");
-    var calculationData = {
-      operator: operator,
-      leftOperand: leftOperand,
-      rightOperand: rightOperand
-    };
-    $.ajax({
-      type: "POST",
-      url: "/calculate",
-      data: calculationData,
-      success: function (res) {
-        console.log(res);
-      }
-    });
-  });
+  appendDOM();
+  addEventListeners();
+  updateDisplay();
 });
+
+function appendDOM() {
+  var numArray = [1,2,3,4,5,6,7,8,9,0];
+  for (var i = 0; i < numArray.length; i++) {
+    $('#numPad').append(createNumberButton(numArray[i]));
+  }
+}
+function createNumberButton(i) {
+  console.log("Creating button ", i);
+  return "<button class='numberButton' id='" + i + "'>" + i + "</button>";
+}
+
+function addEventListeners() {
+  $('#numPad').on('click', '.numberButton', inputNumber);
+  $('.setOperatorButton').on('click', setOperator);
+  $('.calculate').on('click', doCalculation);
+}
+
+function updateDisplay () {
+  getFigletText(assembleDisplayString());
+}
+
+function assembleDisplayString () {
+  var displayString;
+  if (operator) {
+    displayString = leftOperand + operatorToSymbol(operator) + rightOperand;
+  }
+  else if (leftOperand) {
+    displayString = leftOperand;
+  }
+  else {
+    displayString = "0";
+  }
+  return displayString;
+}
+
+function operatorToSymbol (operator) {
+  var operatorSymbol;
+  switch (operator) {
+    case "add":
+      operatorSymbol = "+";
+      break;
+    case "subtract":
+      operatorSymbol = "-";
+      break;
+    case "multiply":
+      operatorSymbol = "X";
+      break;
+    case "divide":
+      operatorSymbol = "/";
+      break;
+  }
+  return operatorSymbol;
+}
+
+function inputNumber() {
+  console.log("input: ", $(this).attr('id'));
+  if (operator) {
+    rightOperand += $(this).attr('id');
+  } else {
+    leftOperand += $(this).attr('id');
+  }
+  updateDisplay();
+}
+
+function setOperator () {
+  console.log("set operator: ", $(this).data("operator"));
+  operator = $(this).data("operator");
+  updateDisplay();
+}
+
+function doCalculation() {
+  if (leftOperand && rightOperand && operator) {
+    postCalculation();
+  } else {
+    alert("Please input all required data");
+  }
+}
+
+function postCalculation() {
+  var calculationData = {
+    operator: operator,
+    leftOperand: parseInt(leftOperand),
+    rightOperand: parseInt(rightOperand)
+  };
+  $.ajax({
+    type: "POST",
+    url: "/calculate",
+    data: calculationData,
+    success: function (res) {
+      leftOperand = res.result;
+      rightOperand = "";
+      operator = undefined;
+      updateDisplay();
+    }
+  });
+}
+
+function getFigletText (figletText) {
+  console.log('get figlet');
+  $.ajax({
+    url: '/figlet',
+    type: 'POST',
+    data: {text: figletText},
+    success: function (response) {
+      $('#calcDisplay pre').text(response);
+    }});
+}
